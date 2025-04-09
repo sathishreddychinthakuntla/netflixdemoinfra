635e68e0a7a7920cf5ec68b75562fac0d85d4d10
sathishreddychinthakuntla <sathishreddych96@gmail.com>
2025-04-09 16:26:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "aws" {
 region = "us-east-1"
 }
 
-resource "aws_instance" "two" {
+resource "aws_instance" "one" {
 count = 5
 ami = "ami-07a6f770277670015"
-instance_type = "t2.medium"
+instance_type = "t2.micro"
 key_name = "myserver"
-vpc_security_group_ids = ["sg-039e0d894a4062929"]
+vpc_security_group_ids = ["sg-0b6cdb3cdf0c0a68b"]
 tags = {
 Name = var.instance_names[count.index]
 }
