64674e5473f76b7f13fa02d0a1d64377907c0fb9
sathishreddychinthakuntla <sathishreddych96@gmail.com>
2025-04-14 18:34:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,16 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
+count = 4
 ami = "ami-07a6f770277670015"
 instance_type = "t2.micro"
 key_name = "myserver"
-vpc_security_group_ids = ["sg-0b6cdb3cdf0c0a68b"]
+vpc_security_group_ids = ["sg-0589cf1c342ef9ece"]
 tags = {
 Name = var.instance_names[count.index]
 }
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "AppServer-1", "AppServer-2", "Monitoring server"]
+default = ["jenkins", "AppServer-1", "AppServer-2", "Monitoring server"]
 }
